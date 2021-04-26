bf750810c2e83762c235a82d532fa71bbbd9f7bf
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-26 19:39:03 -0300

    Exercicio 2

--- Bloco_11_-_Introducao_à_React/dia_1/exercises/src/App.js
+++ Bloco_11_-_Introducao_à_React/dia_1/exercises/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Exercicio 2
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World!</p>
+      <p>{Task(2)}</p>
     </div>
+
   );
 }
 
