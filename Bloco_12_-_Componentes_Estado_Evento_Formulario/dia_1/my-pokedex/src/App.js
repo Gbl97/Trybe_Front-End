3072ecdf4143403df6f1b90e673344f5b50e01f1
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-30 10:29:44 -0300

    Exercicio 1

--- Bloco_12_-_Componentes_Estado_Evento_Formulario/dia_1/my-pokedex/src/App.js
+++ Bloco_12_-_Componentes_Estado_Evento_Formulario/dia_1/my-pokedex/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+      <Pokedex pokemon={pokemons} /> 
+   </div>
   );
 }
 
--- Bloco_12_-_Componentes_Estado_Evento_Formulario/dia_1/my-pokedex/src/components/Pokedex.jsx
+++ Bloco_12_-_Componentes_Estado_Evento_Formulario/dia_1/my-pokedex/src/components/Pokedex.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            position: 0
+        }
+
+        this.nextPokemon = this.nextPokemon.bind(this);
+    }
+
+    nextPokemon() {
+      const { pokemon } = this.props;
+      this.setState((previosState, _props) => {
+          const { position } = previosState;
+          const indexAtual = position === pokemon.length - 1 ? 0 : position + 1;
+          return { ...previosState, position: indexAtual};
+      });
+    };
+
     render() {
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+            <main>
+                <div className="pokedex">
+                <Pokemon  pokemon={this.props.pokemon[this.state.position]} />
+                </div>
+                <button onClick={ this.nextPokemon }>Próximo Pokemon!</button>
+                
+            </main>
         );
     }
-}
+};
 
 export default Pokedex;
